7f58936dee88d945b98ab8f65e4389e03d40d0e0
MARYAM-ALAA <116362194+MARYAM-ALAA@users.noreply.github.com>
2022-12-24 02:24:27 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                Console.WriteLine("Please Enter The First Number Of The Range");
+                int start = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please Enter The Last Number Of The Range");
+                int end = int.Parse(Console.ReadLine());
+
+
+                {
+                    for (int i = start; i <= end; i++)
+                    {
+                        int count = 0;
+                        for (int j = 2; j <= i / 2; j++)
+                        {
+                            if (i % j == 0)
+                            {
+                                count++;
+                                break;
+                            }
+                        }
+                        if (count == 0 && i != 1)
+                            Console.WriteLine(i + " ");
+                    }
+                }
+
+
+               
+
+
+            }
+        }
+
+
+
+        }
+
+
+        }
+    
+
